a6730b6aad2f78e0c8e791a4aa203f7124f99328
Nathan Le <nathanle1@cpp.edu>
2026-03-08 14:31:38 -0700

    Draft for rotation handling

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using UnityEngine;
 
+using Game.PieceUtil;
+
 class Board : MonoBehavior {
 
 	private int width = 10, height = 20;
 	private Cell[,] cells = new Cell[width,height];
-	private PieceName currentPiece, holdPiece;
 	private Queue queue;
+
+	private CurrentPiece current = new CurrentPiece();
+
 
 	// Spawn next piece in queue
 	public void SpawnPiece();
@@ -15,10 +19,10 @@ class Board : MonoBehavior {
 	public void SwapPiece();
 
 	// Move piece horizontally
-	public void Move(Movement dir);
+	public void MovePiece(Movement dir);
 
 	// Rotate piece
-	public void Rotate(Rotation dir);
+	public void RotatePiece(Rotation dir);
 	
 	// Move the piece down faster
 	public void SoftDrop();
@@ -29,6 +33,49 @@ class Board : MonoBehavior {
 	// Move the piece down 1 cell
 	public void ApplyGravity();
 
-	// Handle rotation collision cases
-	public Cell[,] KickOffset();
+	class BoardPiece {
+		string name;
+		int rotation;
+		(int x, int y) position;
+		
+		BoardPiece(Piece target) {
+			name = n;
+			rotation = 0;
+			position = Util.GetDefaultPositionOfPiece(n); // resolve default position of piece
+		}
+
+		void ChangeTo(Piece target) {
+			this(target);
+		}
+	}
+
+	private (bool, int, int) ValidateRotation(int pre, int post) {
+		// get target rotation state data
+		int[,] postData = PieceRotationStateData<I>[post];
+		// lookup kick table		
+		(int,int)[] kickData = Rotation.GetKickData(current.name, pre, post);
+
+		int validCase = -1, offsetX = 0, offsetY = 0;
+		for (int i = 0; i < 5 && validCase <= 0; i++) {
+			(offsetX, offsetY) = kickData[validCase];
+			if (!CollisionWithOffset(postData, offsetX, offsetY))
+				validCase = i;
+		}
+
+		return (validCase != -1, offsetX, offsetY);
+	}
+	
+	private bool CollisionWithOffset(int[,] data, int x, int y) {
+		int candX = current.posX + X;
+		int candY = current.posY + Y;
+
+		// check if cells of rotation must be filled while the corresponding board cell is filled
+		int collided = false;
+		for (int i = 0; i < 4 && !collided; i++)
+			for (int j = 0; j < 4 && !collided; j++)
+				if (data[i][j] == 1 && board.cells[i+candX][j+candY] == 1)
+					collided = true;
+
+		return !collided;
+	}
 }
