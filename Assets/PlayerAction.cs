1e165894eee2de84f8f79bd0138cab307450cc5d
Nathan Le <nathanle1@cpp.edu>
2026-03-08 14:27:20 -0700

    Data for rotation 'kicking' + reorganization

---
+++ Assets/PlayerAction.cs
@@ -0,0 +1,16 @@
+namespace Game.PlayerAction {
+
+	public enum Move {
+		None = 0,
+		Left = -1,
+		Right = 1,
+	}
+
+	public enum Rotate {
+		None = 0,
+		Clockwise = 1,
+		CounterClockwise = -1,
+		Flip = 2
+	}
+
+}
